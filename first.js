9d60aafe7ec270b7acb51faf307c2b26decc91c9
Muhammad Akif Hussain <makifhussainmn@gmail.com>
2024-11-25 02:45:39 +0500

    first  commit

---
+++ first.js
@@ -0,0 +1,152 @@
+document.getElementById("alertname").onclick = function (){
+ let Name = ("Muhammad Akif Husain");
+ let Akif = alert (Name);
+ let combine = Name;
+ console.log(combine);
+document.getElementById("outputbox").innerHTML=(combine)
+
+}
+
+document.getElementById("alertnumber").onclick = function() {
+
+let number = ("03013200651") ;
+let main = alert(number)
+const special = number
+console.log(special)
+document.getElementById("outputbox").innerHTML=(special)
+
+};
+
+document.getElementById("showvariblenumber").onclick = function() {
+
+    let showvariblenumber = +92300787459 ;
+    console.log(showvariblenumber)
+
+    document.getElementById("outputbox").innerHTML=(showvariblenumber)
+    
+    };
+
+    
+
+    document.getElementById("showcanelcase").onclick = function() {
+    
+        let   showcanelcase = "akifIsChudhary" ;
+        console.log( showcanelcase)
+    
+        document.getElementById("outputbox").innerHTML=(showcanelcase)
+        document.getElementById("resultbox").innerHTML=(showcanelcase)
+        
+        };
+  
+  
+        
+
+        document.getElementById("Sum2number").onclick = function() {
+        
+            let   Sum2number = 2+9+100+960+548 ;
+            let Sum2numberbasic = "2+9+100+960+548 "
+            let sumnu = alert (Sum2number)
+            console.log( Sum2number)
+        
+            document.getElementById("outputbox").innerHTML=( Sum2numberbasic)
+            document.getElementById("resultbox").innerHTML=(Sum2number)
+            
+            };
+      
+
+            document.getElementById("Subtracted2number").onclick = function() {
+        
+                let Subtracted2number   = 55-8-5-5-1-0 ;
+                let Subtracted2numberbasic    = "55-8-5-5-1-0" ;
+                let sumnu = alert (Subtracted2number)
+                console.log( Subtracted2number)
+            
+                document.getElementById("outputbox").innerHTML=(Subtracted2numberbasic)
+                document.getElementById("resultbox").innerHTML=(Subtracted2number)
+                
+                };
+
+
+                document.getElementById("Multify2number").onclick = function() {
+        
+                    let   Multify2number   = 2*2*2*5*9*5 ;
+                    let Multify2numberbasic    = " 2*2*2*5*9*5" ;
+                    let sumnu = alert (  Multify2number)
+                    console.log(   Multify2number)
+                
+                    document.getElementById("outputbox").innerHTML=(Multify2numberbasic)
+                    document.getElementById("resultbox").innerHTML=(  Multify2number)
+                    
+                    };
+  
+  
+                    document.getElementById("Divided2number").onclick = function() {
+        
+                        let   Divided2number   = 100/2;
+                        let  Divided2numberbasic    = "5000 / 566 / 2" ;
+                        let diverder = alert (Divided2number)
+                        console.log(Divided2number)
+                    
+                        document.getElementById("outputbox").innerHTML=(Divided2numberbasic)
+                        document.getElementById("resultbox").innerHTML=( Divided2number)
+                        
+                        };
+  
+                    document.getElementById("showstring2").onclick = function() {
+        
+                        let   showstring2   = "Akif" + "Hussain" ;
+                        let  showstring2basic    = "5000 / 566 / 2" ;
+                        let diverder = alert (showstring2)
+                        console.log(showstring2)
+                    
+                        document.getElementById("outputbox").innerHTML=(showstring2basic)
+                        document.getElementById("resultbox").innerHTML=( showstring2)
+                        
+                        };
+
+
+
+
+
+
+
+
+
+                   
+
+
+
+              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+        document.getElementById("ClearStatment").onclick = function(){
+
+document.getElementById("outputbox").innerHTML = ""
+
+   }
+  
+   document.getElementById("clearbutton").onclick = function(){
+
+document.getElementById("resultbox").innerHTML = ""
+
+   }
